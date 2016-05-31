b3f38a6d4845dbbcea0eb579ca89479c1024a04b
jcx <jcxnju@gmail.com>
2016-05-31 16:05:45 +0800

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by jcx on 16/5/31.
+ */
+
+
+$(function() {
+    $("#prevhand").on("click", function(e) {
+        ;
+    });
+    $("#nexthand").on("click", function(e) {
+        ;
+    });
+    $("#prevaction").on("click", function(e) {
+        ;
+    });
+    $("#nextaction").on("click", function(e) {
+        ;
+    });
+    $("#restart").on("click", function(e) {
+        ;
+    });
+});
+
+
